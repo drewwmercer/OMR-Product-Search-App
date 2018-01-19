cc5c5dfc9754cfb435849e7ef9c3a698c219fe61
Drew Mercer <japanexpress@gmail.com>
2018-01-18 21:38:52 -0500

    Adding some placholder search results

--- app/index.js
+++ app/index.js
@@ -4,13 +4,24 @@ const ReactDOM = require('react-dom');
 class Search extends React.Component {
   render() {
     return (
-        <div className="search">
-         <div className="search-bar">
-            <input placeholder="Search" />
-         </div>
-
+      <div className="search">
+        <div className="search-bar">
+          <input placeholder="Search" />
         </div>
-    )
+        <div className="results">
+          <div className="in-stock">
+            <h2>
+              <a href="#">Toothpaste</a>
+            </h2>
+            <p>$2.99</p>
+            <p>
+              I'm a placeholder but I can just paragraph text to show on the
+              item.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
