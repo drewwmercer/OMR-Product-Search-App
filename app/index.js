36b9185d72c87e52fae1488418bc814e92c19c66
Drew Mercer <japanexpress@gmail.com>
2018-01-18 21:33:39 -0500

    Adding search component

--- app/index.js
+++ app/index.js
@@ -1,4 +1,17 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-ReactDOM.render(<h1>Hey Drew</h1>, document.getElementById('app'))
+class Search extends React.Component {
+  render() {
+    return (
+        <div className="search">
+         <div className="search-bar">
+            <input placeholder="Search" />
+         </div>
+
+        </div>
+    )
+  }
+}
+
+ReactDOM.render(<Search />, document.getElementById('app'));
